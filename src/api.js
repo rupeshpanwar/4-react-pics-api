149b2c8199ebdfb592c23db2e5377029034a08f7
Jon Doe <jondoe@doe.com>
2023-02-15 17:21:50 +0800

    step2- add Axios , to query the photos search in Api.js

---
+++ src/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const clientId = 'zjp28aWLta8PBt-lGNLHOxQ5ry1uzguBcduC_BrG7_Q';
+
+const searchImages = async () => {
+  const response = await axios.get('https://api.unsplash.com/search/photos', {
+    headers: {
+      'Authorization': `Client-ID ${clientId}`
+    },
+    params: {
+      query: 'cars'
+    }
+  })
+    .then(response => {
+      //return response.data.results;
+      console.log(response)
+      return response;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
+
+export default searchImages;
+
